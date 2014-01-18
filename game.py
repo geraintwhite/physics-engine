98f9e436d6f74df4f6278bf2f8974f928e477609
Geraint White <geraintwhite@gmail.com>
2014-01-18 17:28:30 +0000

    Added simple ball simulation

--- game.py
+++ game.py
@@ -1,11 +1,40 @@
 from graphics import *
+import time
 
 def main():
-    screen = Canvas()
-    circle = Sprite(shapes.Circle(5),
-                    position = (10, 5))
-    screen.sprites.append(circle)
+    FPS = 30
+
+    screen = Canvas(size = (40, 40))
+
+    ball = Sprite(
+        shapes.Circle(5),
+        position = (14, 10)
+    )
+
+    screen.sprites.append(ball)
+
     print(screen)
+
+    with NonBlockingInput() as nbi:
+        while True:
+
+            if not 0 in ball.edge(screen):
+                ball.move(0)
+
+            ch = nbi.char()
+
+            if ch == '.':
+                if not 3 in ball.edge(screen):
+                    ball.move(3)
+            if ch == ',':
+                if not 1 in ball.edge(screen):
+                    ball.move(1)
+            if ch == ' ':
+                ball.position = (14, 10)
+
+            print(screen)
+
+            time.sleep(1/FPS)
 
 if __name__ == '__main__':
     main()
